7096953c5cd4e15ad2271ecb54462365c8dfe209
Roshan Jobanputra <rosh93@gmail.com>
2014-08-26 11:58:19 -0700

    Upgrade package to support meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "Create a multipart/form-data encoded string. Useful for sending attachments with Mailgun."
+  summary: "Create a multipart/form-data encoded string. Useful for sending attachments with Mailgun.",
+  version: "0.0.2",
+  git: "https://github.com/rosh93/meteor-multipart-form-data.git"
 });
 
 Package.on_use(function(api, where) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files('multipart.js', 'server');
   api.export('MultipartFormData', 'server');
 });
